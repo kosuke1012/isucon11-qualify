c7174eaacbe49d17938b0cc6dc7a790a8407dca7
Kosuke Yamagishi <kosuke.yamagishi@smartdrive.co.jp>
2022-07-20 21:58:52 +0900

    quit loggin

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -212,8 +212,8 @@ func init() {
 
 func main() {
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(log.DEBUG)
+	// e.Debug = true
+	// e.Logger.SetLevel(log.DEBUG)
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
@@ -1268,7 +1268,7 @@ func postIsuCondition(c echo.Context) error {
 	// TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
 	dropProbability := 0.9
 	if rand.Float64() <= dropProbability {
-		c.Logger().Warnf("drop post isu condition request")
+		// c.Logger().Warnf("drop post isu condition request")
 		return c.NoContent(http.StatusAccepted)
 	}
 
